fd935566537056bed11d2c3923c4cc05e53e349b
Deepak Kumar <deep7654k@gmail.com>
2026-02-04 00:42:37 +0530

    add Service to validate the payload for JWT verification

---
+++ server/src/services/user/validateToken.service.ts
@@ -0,0 +1,10 @@
+import jwt from "jsonwebtoken";
+import { z } from "zod";
+
+const validateToken = <T>(token : string , zodSchema : z.ZodType<T> ) :T => {
+    const payload = jwt.verify(token, process.env.JWT_SECRET!)
+    const validatedPayload = zodSchema.parse(payload);
+    return validatedPayload;
+}
+
+export default validateToken;
